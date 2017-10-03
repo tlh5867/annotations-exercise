53737a0d9b80ad103ffeab53ef5913287170cae3
Tomas Hipolito <tlhipolito67@gmail.com>
2017-10-03 09:12:58 -0500

    -used deprcated to get rid of error

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -28,8 +28,8 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Deprecated
-    public String sortName() {
+    @Override
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
